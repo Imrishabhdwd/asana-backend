d2baa593e5309725bcdaa20d28b4b206188e08ae
Rishabh <Imrishabhdwd@gmail.com>
2019-05-03 01:14:49 +0530

    add mlab connections

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-let connection = mongoose.connect('mongodb://localhost/local')
+let connection = mongoose.connect('mongodb://myApp:Imrishabh@123@ds229771.mlab.com:29771/testmj_app')
 .then(() => console.log("mongoose connected!!"))
 .catch(err => console.log("mongoose connection error"))
 
