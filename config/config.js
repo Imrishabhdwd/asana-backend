15323e80810c7d800ad0a7c69101991551605e27
Rishabh <Imrishabhdwd@gmail.com>
2019-05-03 12:59:05 +0530

    Change DBURI

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-let connection = mongoose.connect('mongodb://myApp:Imrishabh@123@ds229771.mlab.com:29771/testmj_app')
+let connection = mongoose.connect('mongodb://myApp:App1234@ds229771.mlab.com:29771/testmj_app')
 .then(() => console.log("mongoose connected!!"))
 .catch(err => console.log("mongoose connection error"))
 
