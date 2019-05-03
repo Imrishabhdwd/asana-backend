6c73071e2d5f00845e870860f40fee600077b2ac
Rishabh <Imrishabhdwd@gmail.com>
2019-05-03 13:09:15 +0530

    Changes in Db Connections

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-let connection = mongoose.connect('mongodb://myApp:App1234@ds229771.mlab.com:29771/testmj_app')
+let connection = mongoose.connect('mongodb://myApp:App1234@ds229771.mlab.com:29771/testmj_app',{ useNewUrlParser: true })
 .then(() => console.log("mongoose connected!!"))
 .catch(err => console.log("mongoose connection error"))
 
